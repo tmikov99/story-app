fbda6e97b69cae02f1458d66b3d9b7df74690cd8
Todor Mikov <tmikov99@gmail.com>
2025-03-23 01:58:00 +0200

    Finish setup with spring initializr

---
+++ src/test/java/com/example/story_app/StoryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.story_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
