a634844004ba34427ba227e62da5cc91f7ed0d71
Todor Mikov <tmikov99@gmail.com>
2025-04-05 20:52:57 +0300

    Add refresh token and clean up APIs

--- src/main/java/com/coursework/story/controller/StoryController.java
+++ src/main/java/com/coursework/story/controller/StoryController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "story")
+@RequestMapping(value = "/api/story")
 public class StoryController {
 
     @Autowired
--- src/main/java/com/coursework/story/repository/RefreshTokenRepository.java
+++ src/main/java/com/coursework/story/repository/RefreshTokenRepository.java
@@ -8,5 +8,6 @@ import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByToken(String token);
+    Optional<RefreshToken> findByUser(User user);
     void deleteByUser(User user);
 }
