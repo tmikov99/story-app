c4c0cd3b4aac445e6a4b7e6684d3cb99e6d2fded
Todor Mikov <61269302+tmikov99@users.noreply.github.com>
2025-05-02 19:00:29 +0300

    Save and update story and related image (#28)

--- src/main/java/com/coursework/story/service/FirebaseStorageService.java
+++ src/main/java/com/coursework/story/service/FirebaseStorageService.java
@@ -4,6 +4,7 @@ import com.google.cloud.storage.Acl;
 import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.Bucket;
 import com.google.firebase.cloud.StorageClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -12,6 +13,12 @@ import java.util.UUID;
 
 @Service
 public class FirebaseStorageService {
+    @Value("${story.app.firebaseStorageBucket}")
+    private String storageBucket;
+
+    public String getBucketName() {
+        return storageBucket;
+    }
 
     public String uploadFile(MultipartFile file, String path) throws IOException {
         String fileName = UUID.randomUUID() + "-" + file.getOriginalFilename();
